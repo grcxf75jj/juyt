bcb76511d7063cb9c8da4e93a585738e4caab995
grcxf75jj <kikicpt80@gmail.com>
2024-06-08 22:01:09 +0700

    Update discord.py

--- discord.py
+++ discord.py
@@ -4,9 +4,7 @@ import time
 
 with open('./config.json') as f:
     config_data = json.load(f)
-    channel_id = config_data['Config'][0]['channelid']  # config.json
     token = config_data['Config'][0]['token']  # config.json
-    message = config_data['Config'][0]['message']  # config.json
 
 header_data = { 
     "Content-Type": "application/json", 
